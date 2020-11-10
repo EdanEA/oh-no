9a6fd6ec1ee11870b1639617b998347a1ebf6240
Edan <EdanEA@users.noreply.github.com>
2020-11-10 01:18:00 -0600

    1.0.3

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const client = new Eris.Client(conf.bot.token);
 
 client.on('ready', () => {
   console.log(`Successfully logged in.\n  As: ${client.user.username}#${client.user.discriminator}\n  Server Count: ${client.guilds.size}\n  Time: ${moment(new Date()).format("hh:mm:ss a")} (GMT ${new Date().getTimezoneOffset() / -60})`);
+
   client.editStatus("dnd", {name: conf.statuses[Math.floor(Math.random() * conf.statuses.length)], type: 1});
+  dbl.postStats(client.guilds.size, 0, client.shards.size);
 });
 
 client.on('error', (e, id) => {
-  console.error(`Error #${id}\n${e.stack}`);
+  console.error(`Error #${id}\n${e.stack}`)
 });
 
 client.on('warn', (message, id) => {
@@ -25,15 +27,21 @@ client.on('messageCreate', async (msg) => {
   if(msg.author.id == client.user.id)
     return;
 
-  if(msg.content == "_info" && msg.author.id == "221740788462256138")
+  if(msg.content == "_info" && msg.author.id == conf.bot.ownerID)
     return msg.channel.createMessage(`Servers: \`${client.guilds.size}\`\nUsers: \`${client.users.size}\`\nPing: \`${new Date - msg.timestamp} ms\``);
-  if(msg.content == "_status" && msg.author.id == "221740788462256138") {
+
+  if(msg.content == "_status" && msg.author.id == conf.bot.ownerID) {
     var status = conf.statuses[Math.floor(Math.random() * conf.statuses.length)];
     await client.editStatus("dnd", {name: status, type: 1});
     return msg.channel.createMessage(`New Status: \`${status}\``);
   }
 
-  var emojiReg = RegExp("(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])|.{1,32}:[0-9]{18}|♥", "g");
+  if(msg.content == "_restart" && msg.author.id == conf.bot.ownerID) {
+    await msg.channel.createMessage(`ahhhhh`);
+    process.exit(); // only restarts if running with pm2
+  }
+
+  var emojiReg = RegExp("[^:]{1,32}:[0-9]{18}|(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])|♥", "g");
 
   if(!emojiReg.test(msg.content))
     return;
@@ -41,15 +49,15 @@ client.on('messageCreate', async (msg) => {
   var emojis = msg.content.match(emojiReg);
 
   for(var e of emojis) {
-	if(!e)
-	  continue;
+    if(!e)
+      continue;
 
-    msg.addReaction(e);
+    try {
+      msg.addReaction(e);
+    } catch (e) {
+      continue;
+    }
   }
 });
 
 client.connect();
-
-setInterval(() => {
-  client.editStatus("dnd", {name: conf.statuses[Math.floor(Math.random() * conf.statuses.length)], type: 1});
-}, 3.6E+6);
